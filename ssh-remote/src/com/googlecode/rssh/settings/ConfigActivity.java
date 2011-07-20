98074bc55b8cb71835122af35fb4157aeda2bf3a
av.novikov <av.novikov@gmail.com>
2011-07-20 17:30:22 +0000

    browse SSH server for media files added

--- ssh-remote/src/com/googlecode/rssh/settings/ConfigActivity.java
+++ ssh-remote/src/com/googlecode/rssh/settings/ConfigActivity.java
@@ -4,12 +4,16 @@ package com.googlecode.rssh.settings;
 import com.googlecode.rssh.R;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.text.TextUtils;
+
+import java.io.IOException;
 
 /**
  * @author Anton Novikov
  */
-public class ConfigActivity extends PreferenceActivity {
+public class ConfigActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
   public static final String PROFILE = "profile_preference";
 
   public static final String HOST_NAME = "host_preference";
@@ -24,5 +28,27 @@ public class ConfigActivity extends PreferenceActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     addPreferencesFromResource(R.xml.settings);
+
+    Preference profilePref = findPreference(PROFILE);
+    profilePref.setOnPreferenceChangeListener(this);
+  }
+
+  @Override
+  public boolean onPreferenceChange(Preference preference, Object newValue) {
+    String prefKey = preference.getKey();
+    if (PROFILE.equals(prefKey)) {
+      String profileRes = (String) newValue;
+      try {
+        if (!TextUtils.isEmpty(profileRes)) {
+          Profile profile = ProfileReader.readProfile(getApplicationContext(), profileRes);
+        } else {
+          // TODO: handle custom profile.
+        }
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    return false;
   }
 }
