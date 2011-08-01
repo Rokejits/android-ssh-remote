b180ba5c18b0a2867305660f9931d095124b40f6
av.novikov@gmail.com <av.novikov@gmail.com@71204074-a17e-c2d5-9081-c6d722b94cce>
2011-08-01 22:05:56 +0000

    fixing small bugs

--- trunk/ssh-remote/src/com/googlecode/rssh/shell/CommunicationService.java
+++ trunk/ssh-remote/src/com/googlecode/rssh/shell/CommunicationService.java
@@ -88,14 +88,16 @@ public class CommunicationService extends Service {
   }
 
   private void connect(Messenger replyTo) throws RemoteException {
-    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    SharedPreferences prefs = PreferenceManager
+        .getDefaultSharedPreferences(getApplicationContext());
     String host = prefs.getString(ConfigActivity.HOST_NAME, "");
     String port = prefs.getString(ConfigActivity.PORT, getString(R.string.defaultPort));
     String username = prefs.getString(ConfigActivity.USER_NAME, "");
     String password = prefs.getString(ConfigActivity.PASSWORD, "");
 
     if (TextUtils.isEmpty(host) || TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
-      Toast.makeText(getApplicationContext(), R.string.connection_not_configured, Toast.LENGTH_SHORT).show();
+      Toast.makeText(getApplicationContext(), R.string.connection_not_configured,
+          Toast.LENGTH_SHORT).show();
     } else {
       boolean connected = shell.login(host, Integer.parseInt(port), username, password);
       Message response = Message.obtain(null, COMMAND_CONNECT, connected ? 1 : 0, 0);
@@ -107,12 +109,14 @@ public class CommunicationService extends Service {
   }
 
   private void showIcon() {
-    Notification notif = new Notification(R.drawable.icon, "", System.currentTimeMillis());
+    Notification notif = new Notification(R.drawable.stat_app_icon, "", System.currentTimeMillis());
     Intent intent = new Intent(starterAction);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
     PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,
         PendingIntent.FLAG_UPDATE_CURRENT);
-    notif.setLatestEventInfo(getApplicationContext(), getString(R.string.app_name), "", contentIntent);
+    notif.flags |= Notification.FLAG_ONGOING_EVENT;
+    notif.setLatestEventInfo(getApplicationContext(), getString(R.string.app_name), "",
+        contentIntent);
     notificationManager.notify(R.id.ssh_connected_icon, notif);
   }
 
